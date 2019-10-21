b50f231b0021b1d35adb43fbebbce8158d284499
0nuh31n0 <heinrichmei13@gmail.com>
2019-10-21 11:56:14 +0300

    Add Vue

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app" class="container">
+    <Header />
+    <Nav msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Nav from './components/Nav.vue'
+import Header from './components/Header.vue'
+export default {
+  name: 'app',
+  components: {
+    Nav, Header
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Gothic+A1|Love+Ya+Like+A+Sister&display=swap');
+#app {
+  font-family: 'Gothic A1', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  display: flex;
+  justify-content: center;
+}
+.container {
+    /* position: relative; */
+    max-width: 860px;
+    height: 100%;
+    width: 100%;
+  
+    background-repeat: no-repeat;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <a href="#index" style="text-decoration: none;"> <h1>NoorProbleem</h1> </a>
+    </div>
+</template>
+<style scoped>
+
+h1 {
+    font-family: Love Ya Like A Sister;
+    font-style: normal;
+    font-weight: normal;
+    color: #244955;
+    font-size: 132px;
+    line-height: 164px;
+    text-align: center;
+    background: linear-gradient(to right,rgba(36,73,85) 20%, transparent 97%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    transform: matrix(0.99, -0.1, 0.11, 0.99, 0, 0);
+}
+</style>
